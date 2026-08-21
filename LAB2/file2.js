de4c7c50542fae5e3c8a7c020367115f700b7a5f
Anuj Gupta <anujgupta6889@gmail.com>
2026-08-21 14:23:41 +0530

    commit

--- LAB2/file2.js
+++ LAB2/file2.js
@@ -1,7 +1,7 @@
 import{readFile} from "fs/promises";
 
 const readData = (filename) =>{
-    const content = await readFile(filename,'utf-8');
+    const content = readFile(filename,'utf-8');
     return content;
 
 
